79f1d03edd3135f3e309efb5eda48a1fad5018ad
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-21 00:58:52 -0800

    Update helprealm.lua

--- plugins/helprealm.lua
+++ plugins/helprealm.lua
@@ -1,6 +1,6 @@
 do
     function run(msg, matches)
-	return 'Realm Commands: \n !creategroup [Name] \n !createrealm [Name] \n !setname [Name] \n !setabout [GroupID] [Text] \n !setrules [GroupID] [Text] \n !lock [GroupID] [setting] \n !unlock [GroupID] [setting] \n !wholist \n !who \n !type \n !kill chat [GroupID] \n !kill realm [RealmID] \n !addadmin [id|username] \n !removeadmin [id|username] \n !list groups \n !list realms \n !log \n !broadcast [text] \n !br [group_id] [text] \n Creator: @MrGhor & @HaWkerGh'
+	return 'Realm Commands: \n !creategroup [Name] \n !createrealm [Name] \n !setname [Name] \n !setabout [GroupID] [Text] \n !setrules [GroupID] [Text] \n !lock [GroupID] [setting] \n !unlock [GroupID] [setting] \n !wholist \n !who \n !type \n !kill chat [GroupID] \n !kill realm [RealmID] \n !addadmin [id|username] \n !removeadmin [id|username] \n !list groups \n !list realms \n !log \n !broadcast [text] \n !bc [group_id] [text] \n Creator: @MrGhor & @HaWkerGh'
 end
 return {
   patterns = {
