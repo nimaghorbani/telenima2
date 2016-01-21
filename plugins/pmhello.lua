5665a3ba6892f8aa090e50622fb608bf3d37ac97
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-21 02:01:28 -0800

    Create pmhello.lua

---
+++ plugins/pmhello.lua
@@ -0,0 +1,14 @@
+do
+    function run(msg, matches)
+	return 'Hi For More Information Send PM To @MrGhor'
+end
+return {
+  patterns = {
+      "^([Hh]ello)$",
+      "^([Hh]elo)$",
+      "^([Hh]i)$,
+      "^(سلام)$,
+      }, 
+  run = run 
+}
+end
