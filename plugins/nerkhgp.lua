ad43d4e45206e7b137d41d6d8635da183cc0d9f6
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-21 01:56:37 -0800

    Create nerkhgp.lua

---
+++ plugins/nerkhgp.lua
@@ -0,0 +1,12 @@
+do
+    function run(msg, matches)
+	return 'Nerkh Sakht GP \n Mahiyane : 2000 Toman \n Daemi : 5000 Toman \n 15 Rooz Eshterak Avvaliye Raygan \n  Creator: @MrGhor & @HaWkerGh'
+end
+return {
+  patterns = {
+      "^[/!%&$]([Nn]erkhgp)$",
+      "^([Nn]erkhgp)$"
+      }, 
+  run = run 
+}
+end
