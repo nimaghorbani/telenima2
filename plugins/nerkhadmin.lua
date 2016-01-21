5f918a8b9149f252c530e7ee4fbb302c22fa0f1c
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-21 01:53:57 -0800

    Create nerkhadmin.lua

---
+++ plugins/nerkhadmin.lua
@@ -0,0 +1,12 @@
+do
+    function run(msg, matches)
+	return 'Nerkh Admin Mahiyane \n 2000 Toman Kart Sharj \n Daemi 10000 Toman Kart b Kart \n  Creator: @MrGhor & @HaWkerGh'
+end
+return {
+  patterns = {
+      "^[/!%&$]([Nn]erkhadmin)$",
+      "^([Nn]erkhadmin)$"
+      }, 
+  run = run 
+}
+end
