5d96fbe2d31bad944d1753154191ac7850f74f5e
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-20 23:35:20 -0800

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -164,13 +164,13 @@ end
 return {
   description = "Plugin to manage other plugins. Enable, disable or reload.", 
   usage = {
-    "!plugins: list all plugins.", 
-    "!plugins enable [plugin]: enable plugin.",
-    "!plugins disable [plugin]: disable plugin.",
-    "!plugins disable [plugin] chat: disable plugin only this chat.",
-    "!plugins reload: reloads all plugins." },
+    "plugins: list all plugins.", 
+    "plugins enable [plugin]: enable plugin.",
+    "plugins disable [plugin]: disable plugin.",
+    "plugins disable [plugin] chat: disable plugin only this chat.",
+    "plugins reload: reloads all plugins." },
   patterns = {
-    "^!plugins$",
+    "^([Pp]lugins)$",
     "^!plugins? (enable) ([%w_%.%-]+)$",
     "^!plugins? (disable) ([%w_%.%-]+)$",
     "^!plugins? (enable) ([%w_%.%-]+) (chat)",
