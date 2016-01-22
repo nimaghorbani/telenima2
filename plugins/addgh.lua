a1ce7a3356747977f49172cf0e9c48a89a687516
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-22 03:57:41 -0800

    Update addgh.lua

--- plugins/addgh.lua
+++ plugins/addgh.lua
@@ -1,27 +1,28 @@
 do
-local function callbackres(extra, success, result) 
+  
+local function callbackres(extra, success, result) -- Callback for res_user in line 27
   local user = 'user#id'..132921618
-  local chat = 'chat#id'..extra.chatid
+	local chat = 'chat#id'..extra.chatid
+	chat_add_user(chat, user, ok_cb, false)
 end
 function run(msg, matches)
   local data = load_data(_config.moderation.data)
   if not is_realm(msg) then
     if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin(msg) then
-      return 'Group is private.'
+		  return 'Group is private.'
     end
-  end
+end
   if msg.to.type ~= 'chat' then 
     return
   end
   if not is_momod(msg) then
     return
-  end
-return {
+  end 
+  return {
     patterns = {
-      "^[!/$&#@]addgh (.*)$",
+      "^[!/$&%]([Aa]ddgh) (.*)$",
       "^([Aa]ddgh (.*)$"
     },
     run = run
 }
-
 end
