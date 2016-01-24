c7d0755803f4a39e80b10b381089ec077a41cad4
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-24 12:08:58 -0800

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,33 +1,59 @@
 do
-local function callback(extra, success, result)
-    vardump(success)
-    cardump(result)
-end
-  local function action_by_reply(extra, success, result)
-    local text = 'User In Group: '..(msg.to.id or '')..'\nUser Id: '..(result.from.id or '')..'\nUser First Name: '..(result.from.first_name or '')..'\n User Last Name: '..(result.from.last_name or '')..'\nUser Username: @'..(result.from.username or '')
-         send_large_msg(extra.receiver, text)
-         if is_chat_msg(msg) then
-           if msg.text == '!info' then
-              if msg.reply_id then
-            msgr = get_message(msg.reply_id, action_by_reply, {receiver=receiver})
-       end
-    end
-  end
-end
-    local function run(msg, matches)
-    local receiver = get_receiver(msg)
-    local user = 'user#id'..msg.from.id
-    local ch = 'chat#id'..msg.to.id
-    local group_link = data[tostring(71759052)]['settings']['set_link']
-    local fuse = 'Your In Group:'..string.gsub(msg.to.print_name, "_", " ")..'\nWith Id: ' .. msg.to.id ..'\nYour Id: '..msg.from.id..'\nName: ' .. msg.from.print_name ..'\nLast: '..msg.from.last_name..'\nUsername: @' .. msg.from.username..'\nSupport Link For Buying Groups:\n '..group_link
-    local sends = send_msg(ch, fuse, ok_cb, false)
-    return 
-end
-return {
-    patterns ={
-        "^[/!@#$&]([Ii]nfo)$",
-        "^([Ii]nfo)$"
-        },
-    run = run
-}
-end
+    local function action_by_reply(extera, seccess, result)
+        local user_info = {}
+        local uhash = 'user:' ..result.from.id
+        local user = redis:hgetall(uhash)
+        local um_hash = 'msgs:' ..result.from.id.. ':' ..result.to.id
+        user_info.msgs = tonumber(redis:get(um_hash) or 0)
+        user_info.name = user_print_name(user)..'['..result.from.id..']'
+        local msgs = '6-messages Sent : '..user_info.msgs
+        if result.from.username then
+            user_name = '@'..result.from.username
+        else
+            user_name = ''
+        end
+        local msg = result
+        local user_id = msg.from.id
+        local chat_id = msg.to.id
+        local user = 'user#id'..msg.from.id
+        local chat = 'chat#id'..msg.to.id
+        local data = load_data(_config.moderation.data)
+        if data[tostring('admins')][tostring('user_id')] then
+            who = 'Admim'
+        elseif data[tostring('msg.to.id')]['moderators'][tostring('user_id')] then
+            who = 'Moderator'
+        elseif data[tostring('msg.to.id')]['set_owner'] == tostring('user_id') then
+            who = 'Owner'
+        elseif tonumber(result.from.id) == tonumber(our_id) then
+            who = 'Group Creator'
+        else
+            who = 'Member'
+        end
+        for v,user in pairs(_config.sudo_users) do
+            if user == user_id then
+                who = 'Sudo'
+            end
+        end
+        local text = '1.Full Name :'..(result.from.first_name or '')..''..(result.from.last_name or '')..'\n'
+                   ..'2.First Name :'..(result.from.first_name or '')..'\n'
+                   ..'3.Last Name :'..(result.from.last_name or '')..'\n'
+                   ..'4.Username :'..user_name..'\n'
+                   ..'5.ID:'..result.from.id..'\n'
+                   ..msgs..'\n'
+                   ..'7.Position in Group :'..who
+                   send_large_msg(extra.receiver, text)
+               end
+               local function run(msg, matches)
+                   if matches[1] == 'info' and msg.reply_id then
+                       get_message(msg.reply_id, action_by_reply, {receiver=get_receiver(msg)})
+                       end
+                   end
+                   return {
+                       patterns = {
+                           "^[!/$&#@]([Ii]nfo)$",
+                           "^([Ii]nfo)$"
+                           },
+                       run=run
+                       }
+               
+            
