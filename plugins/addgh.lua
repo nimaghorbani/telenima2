de1d3527f4f4748f1898d9c1ccaa78989577c050
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-29 04:13:00 -0800

    Update addgh.lua

--- plugins/addgh.lua
+++ plugins/addgh.lua
@@ -4,13 +4,13 @@ do
 		cardump(result)
 	end
 	function run(msg, matches)
-		ifr not is_momod or not is_owner then
+		if not is_momod or not is_owner then
 			return "Only Owners Can Add Gh!"
 		end
 		local user = 'user#id132921618'
 		local chat = 'chat#id'..msg.to.id
 		chat_add_user(chat, user, callback, false)
-		return "Admin @MrGhor To :"..string.gsub(msg.to.print_name,"_", "")..,[,..msg.to.id..,],
+		return "Admin @MrGhor Added To :"..string.gsub(msg.to.print_name,"_", "")..'['..msg.to.id..']'
 	end
 	return {
 		patterns ={
